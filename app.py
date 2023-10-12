a927d4ca29b1a84dc64ac4ec7743a8695d1d7b70
Jasmin Shah <jasmin_shah@live.com>
2023-10-13 00:26:03 +0100

    disable debug mode and set port variable

--- app.py
+++ app.py
@@ -58,5 +58,9 @@ def send_cmd(driver, cmd, params={}):
 def download(filename):
     return send_from_directory(os.getcwd(), filename, as_attachment=True)
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
